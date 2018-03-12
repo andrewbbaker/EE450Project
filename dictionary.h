e8190e7978ff9bba1808e637ab7e58ed3bcf8d99
Andrew <andrewbbaker@gmail.com>
2018-03-12 12:30:46 -0700

    implementations for dictionary

--- dictionary.h
+++ dictionary.h
@@ -4,6 +4,7 @@
 
 #include <string.h>
 #include <iostream>
+#include <set>
 
 using namespace std;
 
@@ -16,11 +17,14 @@ class dictionary {
 private:
     string word;
     string definition;
-    dictionary* dictionaries;
+    dictionary** dictionaries;
     static int DICTIONARY_SIZE final = 26;
     void put(string, string, int);
     string get(string, int);
     string remove(string, int);
+    int getLetterIndex(char);
+    set<string> suffix(string, int);
+    void addAllSuffixes(set<string>*);
 
 public:
     dictionary();
@@ -28,45 +32,8 @@ public:
     void put(string, string);
     string get(string);
     string remove(string);
+    set<string> suffix(string);
 };
-
-dictionary :: dictionary() {
-    word = nullptr;
-    definition = nullptr;
-    dictionaries = new dictionary[26];
-
-    for (int i = 0; i < DICTIONARY_SIZE; i++) {
-        dictionaries[i] = nullptr;
-    }
-}
-
-dictionary ::~dictionary() {
-
-}
-
-void dictionary :: put(string word, string definition) {
-    put(word, definition, word.size());
-}
-
-void dictionary :: put(string word, string definition, int currentIndex) {
-
-}
-
-string dictionary :: get(string word) {
-    return get(word, word.size());
-}
-
-string dictionary :: get(string word, int currentIndex) {
-
-}
-
-string dictionary :: remove(string word) {
-
-}
-
-string dictionary :: remove(string word, int currentIndex) {
-
-}
 
 
 
